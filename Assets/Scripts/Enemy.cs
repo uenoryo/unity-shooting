8ba1eb2baa15a9b2e8d1b21547f774499460ef1c
uenoryo <uenoryo@uma-b.com>
2019-06-09 14:58:44 +0900

    敵を召喚.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Spaceship spaceship;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spaceship = GetComponent<Spaceship> ();
+
+        spaceship.Move (transform.up * -1);
+    }
+}
