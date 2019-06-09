267ee1b6edd23732a220c5849012e14f9f8021e0
uenoryo <uenoryo@uma-b.com>
2019-06-09 21:32:42 +0900

    add DestroyArea.

---
+++ Assets/Scripts/Destroyarea.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyarea : MonoBehaviour
+{
+    void OnTriggerExit2D (Collider2D c)
+    {
+        Destroy (c.gameObject);
+    }
+}
