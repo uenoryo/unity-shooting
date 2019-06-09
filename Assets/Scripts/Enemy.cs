8a93981b82ac0c0b481389fa68555b1ee2bb8f10
uenoryo <uenoryo@uma-b.com>
2019-06-09 15:20:12 +0900

    3way追加.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,10 +7,21 @@ public class Enemy : MonoBehaviour
     Spaceship spaceship;
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
         spaceship = GetComponent<Spaceship> ();
 
         spaceship.Move (transform.up * -1);
+
+        while (true) {
+
+            // 子要素を全て取得する
+            for (int i = 0; i < transform.childCount; i++) {
+                Transform shotPosition = transform.GetChild(i);
+                spaceship.Shot (shotPosition);
+            }
+
+            yield return new WaitForSeconds (spaceship.shotDelay);
+        }
     }
 }
