f0e047ef2aab7b679319e846580d21f02ecaa313
uenoryo <uenoryo@uma-b.com>
2019-06-09 14:10:04 +0900

    チュートリアル1まで.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+
+    // Update ...
+    void Update()
+    {
+        float x = Input.GetAxisRaw ("Horizontal");
+        float y = Input.GetAxisRaw ("Vertical");
+
+        Vector2 direction = new Vector2 (x, y).normalized;
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}
