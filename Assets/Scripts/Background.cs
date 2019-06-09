4a98680507c836c2fc70b828e83b9418301cacaf
uenoryo <uenoryo@uma-b.com>
2019-06-09 23:48:40 +0900

    add background.

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float speed= 0.1f;
+
+    void Update()
+    {
+        float y = Mathf.Repeat(Time.time * speed, 1);
+
+        Vector2 offset = new Vector2 (0, y);
+
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
+    }
+}
