cc3475617f19a3beff3d257007c026e51e8a88e4
uenoryo <uenoryo@uma-b.com>
2019-06-09 14:21:27 +0900

    弾を飛ばしてみる.

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float speed = 10;
+
+    // Update ...
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+    }
+}
