2b12a41a8182b1d83f98050ad550a36d9015e395
uenoryo <uenoryo@uma-b.com>
2019-06-09 14:43:13 +0900

    add SpaceShip script.

---
+++ Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SpaceShip : MonoBehaviour
+{
+    public float speed;
+
+    public float shotDelay;
+
+    public GameObject bullet;
+
+    public void Shot (Transform origin)
+    {
+        Instantiate (bullet, origin.position, origin.rotation);
+    }
+
+    public void Move (Vector2 direction)
+    {
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}
